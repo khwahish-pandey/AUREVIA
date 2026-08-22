8ca0c847f6c00ac3f92bba9272d979ffa6021e3a
khwahish-pandey <khwahishpandey7766@gmail.com>
2026-08-22 22:48:26 +0530

    Update home.jsx

--- admin/src/pages/home.jsx
+++ admin/src/pages/home.jsx
@@ -1,25 +1,23 @@
-import React from 'react'
-import { AdminNavbar } from './nav'
+import React from "react";
+import { AdminNavbar } from "./nav";
 import { Routes, Route } from "react-router-dom";
-import AddItem from './Page1'
-import ProductList from './Page2'
 
-import Orders from './AdminOrders'
-
+import AddItem from "./Page1";
+import ProductList from "./Page2";
+import Orders from "./AdminOrders";
 
 function Home() {
   return (
     <div>
-       <AdminNavbar />
-     <Routes>
-      <Route path="/orders" element={<Orders/>}/>
-      <Route path="/page1" element={<AddItem />} />
-       <Route path="/page2" element={<ProductList />} />
-       
-     </Routes>
-     
+      <AdminNavbar />
+
+      <Routes>
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/page1" element={<AddItem />} />
+        <Route path="/page2" element={<ProductList />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
