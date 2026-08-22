dc124c4e8a6437001794f7a22337a4918ff7025d
khwahish-pandey <khwahishpandey7766@gmail.com>
2026-08-22 20:34:50 +0530

    Update orderModel.js

--- backend/model/orderModel.js
+++ backend/model/orderModel.js
@@ -37,6 +37,20 @@ const orderSchema = new mongoose.Schema(
             type:Boolean,
             default: false
         },
+        razorpayOrderId: {
+    type: String,
+    default: null
+},
+
+razorpayPaymentId: {
+    type: String,
+    default: null
+},
+
+razorpaySignature: {
+    type: String,
+    default: null
+},
         date:{
             type: Number,
             default: Date.now
