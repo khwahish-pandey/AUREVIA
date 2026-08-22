b6895e98d9436906f06f7a14692b3da2ab07897d
khwahish-pandey <khwahishpandey7766@gmail.com>
2026-08-22 23:15:25 +0530

    Update productroute.js

--- backend/routes/productroute.js
+++ backend/routes/productroute.js
@@ -1,16 +1,33 @@
 import express from "express";
 import upload from "../middleware/multer.js";
-import  {addProduct} from "../controller/productController.js";
-import e from "express";
-let productRouter=express.Router();
 import adminAuth from "../middleware/adminAuth.js";
-import { listProduct, removeProduct } from "../controller/productController.js";
-productRouter.post("/addproduct",upload.fields([{ name: 'image1', maxCount: 1 },
-     { name: 'image2', maxCount: 1 }, 
-     { name: 'image3', maxCount: 1 }, 
-     { name: 'image4', maxCount: 1 }])
-     ,addProduct);
- productRouter.get("/listproduct",listProduct);
- productRouter.post("/removeproduct/:id",adminAuth,removeProduct);
 
-export default productRouter;    
+import {
+  addProduct,
+  listProduct,
+  removeProduct,
+} from "../controller/productController.js";
+
+const productRouter = express.Router();
+
+productRouter.post(
+  "/addproduct",
+  adminAuth,
+  upload.fields([
+    { name: "image1", maxCount: 1 },
+    { name: "image2", maxCount: 1 },
+    { name: "image3", maxCount: 1 },
+    { name: "image4", maxCount: 1 },
+  ]),
+  addProduct
+);
+
+productRouter.get("/listproduct", listProduct);
+
+productRouter.post(
+  "/removeproduct/:id",
+  adminAuth,
+  removeProduct
+);
+
+export default productRouter;
